60c24da96507bde492074390fe723d294e98b70a
Abigail Gay <abigailg_today@yahoo.com>
2022-03-28 21:12:41 -0500

    Delet all JS code. Pseudo coded again. Rewrote JS code. Added HTML tags

---
+++ rock-paper-scissors.js
@@ -0,0 +1,53 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let compterChoice;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (choice) => {
+    userChoice = choice.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    getComputerChoice();
+    getResult();
+}))
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock';
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors';
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'You Win!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'You Lose!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'You Win!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = ' You Lose!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'You Win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'You Lose!'
+    }
+    resultDisplay.innerHTML = result;
+}
